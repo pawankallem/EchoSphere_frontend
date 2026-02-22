009984f29765f27a022c68a90d37a17c428bf51e
pavankallempinaca <pavan.k@pinacagroup.com>
2026-02-22 18:14:59 +0530

    UI bugs resolved where single selected model not looking good in mobile view.

--- src/components/PostModal.jsx
+++ src/components/PostModal.jsx
@@ -1,11 +1,12 @@
 import { useEffect } from "react";
 import { X, Heart, Bookmark } from "lucide-react";
 import Comments from "./Comments";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleLike, toggleSave } from "../features/posts/postSlice";
 
 export default function PostModal({ post, onClose }) {
     const dispatch = useDispatch();
+    const { user } = useSelector((s) => s.auth);
 
     useEffect(() => {
         const esc = (e) => e.key === "Escape" && onClose();
@@ -25,30 +26,30 @@ export default function PostModal({ post, onClose }) {
             <div
                 onClick={(e) => e.stopPropagation()}
                 className="
-          relative
-          w-full h-full
-          md:w-[95vw] md:h-[92vh]
-          max-w-[1600px]
-          bg-white dark:bg-gray-950
-          md:rounded-2xl
-          shadow-2xl
-          overflow-hidden
-          flex flex-col md:flex-row
-        "
+                    relative
+                    w-full h-full
+                    md:w-[95vw] md:h-[92vh]
+                    max-w-[1600px]
+                    bg-white dark:bg-gray-950
+                    md:rounded-2xl
+                    shadow-2xl
+                    flex flex-col md:flex-row
+                    overflow-hidden
+                "
             >
                 <button
                     onClick={onClose}
                     className="
-            absolute top-4 right-4 z-10
-            bg-black/40 hover:bg-black/60
-            text-white p-2 rounded-full
-            backdrop-blur
-          "
+                        absolute top-4 right-4 z-10
+                        bg-black/40 hover:bg-black/60
+                        text-white p-2 rounded-full
+                        backdrop-blur
+                    "
                 >
                     <X size={22} />
                 </button>
 
-                <div className="flex-1 bg-white flex items-center justify-center">
+                <div className="flex-1 p-4 bg-white flex items-center justify-center">
                     <img
                         src={post.image}
                         alt="post"
@@ -57,12 +58,15 @@ export default function PostModal({ post, onClose }) {
                 </div>
 
                 <div className="
-          w-full md:w-[440px]
-          border-t md:border-t-0 md:border-l
-          border-gray-200 dark:border-gray-800
-          flex flex-col
-          bg-white dark:bg-gray-950
-        ">
+                    flex-1
+                    overflow-y-auto
+                    w-full md:w-[440px]
+                    border-t md:border-t-0 md:border-l
+                    border-gray-200 dark:border-gray-800
+                    flex flex-col
+                    bg-white dark:bg-gray-950
+                    "
+                >
 
                     <div className="flex items-center gap-3 p-5 border-b border-gray-200 dark:border-gray-800">
                         <img
@@ -92,7 +96,11 @@ export default function PostModal({ post, onClose }) {
                             onClick={() => dispatch(toggleLike(post._id))}
                             className="flex items-center gap-2 text-gray-600 dark:text-gray-300 hover:text-red-500 transition"
                         >
-                            <Heart size={24} />
+                            <Heart
+                                size={22}
+                                fill={post?.likesCount > 0 && (post?.likedBy?.includes(user?.id)) ? "red" : "none"}
+                                color={post?.likesCount > 0 && post?.likedBy?.includes(user?.id) ? "red" : "currentColor"}
+                            />
                             <span className="text-sm font-medium">{post.likesCount}</span>
                         </button>
 
@@ -100,12 +108,15 @@ export default function PostModal({ post, onClose }) {
                             onClick={() => dispatch(toggleSave(post._id))}
                             className="flex items-center gap-2 text-gray-600 dark:text-gray-300 hover:text-blue-500 transition"
                         >
-                            <Bookmark size={24} />
+                            <Bookmark
+                                size={22}
+                                fill={post?.savedCount > 0 && post?.savedByUsers?.includes(user?.id) ? "currentColor" : "none"}
+                            />
                             <span className="text-sm font-medium">{post.savedCount}</span>
                         </button>
                     </div>
 
-                    <div className="flex-1 overflow-y-auto px-5 py-4">
+                    <div className="px-5 py-4 mb-10 md:mb-0">
                         <Comments postId={post._id || post.id} />
                     </div>
                 </div>
